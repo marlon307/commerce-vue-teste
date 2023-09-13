cbadb7349d1a376ba7fcbb0993dbf200c2fa238a
marlon307 <marlon-ramosb@hotmail.com>
2023-09-13 18:10:51 -0300

    FEAT: list products api

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,64 @@
 <script setup lang="ts">
 import CardProduct from "@/components/CardProduct.vue";
-
 </script>
 
 <template>
   <h1>Produtos</h1>
   <section class="flex flex-wrap justify-center gap-4">
-    <CardProduct />
-    <CardProduct />
-    <CardProduct />
-    <CardProduct />
+    <CardProduct v-for="product in $data.products" :key="product.id" v-bind="product" />
   </section>
 </template>
+
+<script lang="ts">
+// https://dummyjson.com/docs/products
+interface IProduct {
+  id: number,
+  title: string,
+  description: string,
+  price: number,
+  discountPercentage: number,
+  rating: number,
+  stock: number,
+  brand: string,
+  category: string,
+  thumbnail: string
+  images: string[]
+}
+
+type DataProduct = {
+  loading: boolean;
+  products: IProduct[] | null;
+  error: string | null;
+}
+
+export default {
+  data(): DataProduct {
+    return {
+      loading: false,
+      products: null,
+      error: null,
+    }
+  },
+  created() {
+    // watch the params of the route to fetch the data again
+    this.$watch(
+      () => this.$route.params,
+      () => {
+        this.fetchData()
+      },
+      // fetch the data when the view is created and the data is
+      // already being observed
+      { immediate: true }
+    )
+  },
+  methods: {
+    async fetchData() {
+      this.error = this.products = null;
+      this.loading = true
+      const res = await fetch('https://dummyjson.com/products?limit=10')
+      const json = await res.json();
+      this.products = json.products;
+    },
+  },
+}
+</script>
