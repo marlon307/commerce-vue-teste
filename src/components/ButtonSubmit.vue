34dd5ee472fe374cede220d87e8f2e47172d4839
marlon307 <marlon-ramosb@hotmail.com>
2023-09-18 18:12:46 -0300

    UP: method request login

--- src/components/ButtonSubmit.vue
+++ src/components/ButtonSubmit.vue
@@ -17,9 +17,9 @@ function clickLogin() {
 <template>
   <button
     class="group ml-auto mt-4 flex w-max cursor-pointer items-center justify-center rounded-lg bg-black px-4 py-2 text-sm font-medium text-white hover:bg-black/80"
-    type="submit" aria-label="Salvar" data-loading="false" formmethod="POST" @click="clickLogin">
-    <span class="pointer-events-none absolute group-data-[loading=false]:opacity-0"><svg class="h-4 w-4 animate-spin"
-        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+    type="submit" aria-label="Salvar" data-loading="false" formmethod="POST">
+    <span class="pointer-events-none absolute group-data-[loading=false]:opacity-0">
+      <svg class="h-4 w-4 animate-spin" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
         <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4" />
         <path class="opacity-75" fill="currentColor"
           d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z" />
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,12 +1,28 @@
 <script setup lang="ts">
 import ButtonSubmit from '@/components/ButtonSubmit.vue';
 import FieldInput from '@/components/FieldInput.vue';
+async function onSubmitLogin(e: FormDataEvent) {
+  try {
+    const formData = new FormData(e.target as HTMLFormElement);
+    const res = await fetch('http://127.0.0.1:8000/api/user/login', {
+      headers: {
+        'accept': 'application/json'
+      },
+      method: 'POST',
+      body: formData
+    });
+    const json = await res.json();
+    console.log(json);
+  } catch (error) {
+    console.log(error);
+  }
+}
 </script>
 
 <template>
   <section class="m-auto h-[90vh] max-w-xs flex items-center justify-center flex-col gap-7">
     <h1 class="text-4xl font-bold">Login</h1>
-    <form action="" method="post" class="w-full" @submit.prevent>
+    <form @submit="onSubmitLogin" method="post" class="w-full" @submit.prevent>
       <FieldInput ariaLabel="E-mail" id="email" name="email" placeholder="email@email.com" required type="email" />
       <FieldInput ariaLabel="Senha" id="password" name="password" placeholder="Digite sua senha" required
         type="password" />
